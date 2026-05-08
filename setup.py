a194519f46151c0e4c2baec7dc1cf38b48167c5f
Anuj Verma <72166368+vermaanuj15@users.noreply.github.com>
2026-05-08 16:31:20 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='ML-Project',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Anuj',
+author_email='vermaanuj15@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
